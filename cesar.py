d6468891e75c838a784c9ceb28ed59f9b947a40e
Cristian Cala <cristiancala@protonmail.com>
2021-07-14 00:13:33 -0400

    Cifrado Cesar
    Cifrado Cesar

---
+++ cesar.py
@@ -0,0 +1,22 @@
+posicion = 3
+
+texto = "HOLA AMIGO"
+
+dato_cifrado = ""
+
+for c in texto:
+
+    if c.isupper():
+        c_unicode = ord(c)
+        indice = ord(c) - ord("A")
+        nueva_pos = (indice + posicion) % 26
+        nuevo_unicode = nueva_pos + ord("A")
+        nuevo_caracter = chr(nuevo_unicode)
+        dato_cifrado = dato_cifrado + nuevo_caracter
+    else:
+
+        dato_cifrado += c
+        
+print("Texto:",texto)
+
+print("Texto Encriptado:",dato_cifrado)
